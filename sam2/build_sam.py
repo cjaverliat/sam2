54bfe740d6a38a7bfa52a7baeaba438b60e9e230
Charles JAVERLIAT <charles.javerliat@gmail.com>
2025-05-03 19:29:22 +0200

    Add build_sam2_generic

--- sam2/build_sam.py
+++ sam2/build_sam.py
@@ -13,13 +13,14 @@ from hydra.utils import instantiate
 from omegaconf import OmegaConf
 
 import sam2
+
+from sam2.modeling.sam2_generic import SAM2Generic
 from sam2.sam2_generic_video_memory import (
     DefaultMemoryMemorizationStrategy,
     DefaultMemorySelectionStrategy,
     MemoryMemorizationStrategy,
     MemorySelectionStrategy,
 )
-
 from sam2.sam2_generic_video_predictor import SAM2GenericVideoPredictor
 
 # Check if the user is running Python from the parent directory of the sam2 repo
@@ -96,6 +97,42 @@ def build_sam2(
         ]
     # Read config and init model
     cfg = compose(config_name=config_file, overrides=hydra_overrides_extra)
+    OmegaConf.resolve(cfg)
+    model = instantiate(cfg.model, _recursive_=True)
+    _load_checkpoint(model, ckpt_path)
+    model = model.to(device)
+    if mode == "eval":
+        model.eval()
+    return model
+
+
+def build_sam2_generic(
+    config_file,
+    ckpt_path=None,
+    device="cuda",
+    mode="eval",
+    hydra_overrides_extra=[],
+    apply_postprocessing=True,
+) -> SAM2Generic:
+    hydra_overrides = [
+        "++model._target_=sam2.sam2_generic_video_predictor.SAM2Generic",
+    ]
+    if apply_postprocessing:
+        hydra_overrides_extra = hydra_overrides_extra.copy()
+        hydra_overrides_extra += [
+            # dynamically fall back to multi-mask if the single mask is not stable
+            "++model.sam_mask_decoder_extra_args.dynamic_multimask_via_stability=true",
+            "++model.sam_mask_decoder_extra_args.dynamic_multimask_stability_delta=0.05",
+            "++model.sam_mask_decoder_extra_args.dynamic_multimask_stability_thresh=0.98",
+            # the sigmoid mask logits on interacted frames with clicks in the memory encoder so that the encoded masks are exactly as what users see from clicking
+            "++model.binarize_mask_from_pts_for_mem_enc=true",
+            # fill small holes in the low-res masks up to `fill_hole_area` (before resizing them to the original video resolution)
+            "++model.max_hole_area=8",
+        ]
+    hydra_overrides.extend(hydra_overrides_extra)
+
+    # Read config and init model
+    cfg = compose(config_name=config_file, overrides=hydra_overrides)
     OmegaConf.resolve(cfg)
     model = instantiate(cfg.model, _recursive_=True)
     _load_checkpoint(model, ckpt_path)
