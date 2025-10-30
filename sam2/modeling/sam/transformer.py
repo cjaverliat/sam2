8e7780d6a4ddc332c3e280d7a0f882e4405834cd
Charles JAVERLIAT <charles.javerliat@gmail.com>
2025-10-30 16:08:35 +0100

    Init freqs_cis on CUDA if available

--- sam2/modeling/sam/transformer.py
+++ sam2/modeling/sam/transformer.py
@@ -330,7 +330,9 @@ class EfficientRoPEAttention1(Attention):
             compute_axial_cis, dim=self.internal_dim // self.num_heads, theta=rope_theta
         )
         freqs_cis = self.compute_cis(end_x=feat_sizes[0], end_y=feat_sizes[1])
-        self.freqs_cis = freqs_cis
+        self.freqs_cis = (
+            freqs_cis.to("cuda") if torch.cuda.is_available() else freqs_cis
+        )
         self.rope_k_repeat = rope_k_repeat
 
     def forward(
